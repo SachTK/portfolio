b5c0ab4843b87d9826cb0db1b84bc1fcec2ffb0c
IT21181856 <it21181856@my.sliit.lk>
2023-06-21 13:46:13 +0530

    chancged

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { FaLinkedin, FaGithub, FaFacebookF } from 'react-icons/fa';
+import Typical from 'react-typical'
 
 import bg from "../assets/bg.jpg";
 
@@ -12,11 +13,12 @@ const Main = () => {
         alt="/" 
       /> */}
       <div className='w-full h-screen absolute top-0 left-0'>
-        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-          <h1 className='text-8xl font-bold text-white'>Hi, I'm </h1>
-          <h1 className='text-7xl font-bold text-white'>Sachini Kandambige </h1>
-          <h2 className='flex sm:text-xl text-4xl pt-5 text-sky-400'>
-            <TypeAnimation
+        
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className="text-2xl font-po text-[#00dbfe]">Hi, my name is</p>
+                <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f5]">SACHINI KANDAMBIGE</h1>
+                <h2 className="font- text-2xl sm:text-5xl font-bold text-[#888fa4]">
+                <TypeAnimation
               sequence={[
                 'Developer',
                 2000,
@@ -27,23 +29,26 @@ const Main = () => {
               ]}
               wrapper="div"
               speed={50}
-              style={{ fontSize: '2em', paddingLeft: '5px', display: 'inline-block' }}
+              style={{ fontSize: '40px', paddingLeft: '5px', display: 'inline-block' }}
               repeat={Infinity}
               cursor={true}
             />
-          </h2>
-          <div className='flex justify-between pt-6 max-w-[200px] w-full'>
-            <a href="https://www.linkedin.com/in/sachini-kandambige/" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin className='cursor-pointer mr-6' size={60} color='white' />
-            </a>
-            <a href="https://github.com/IT21181856" target="_blank" rel="noopener noreferrer">
-              <FaGithub className='cursor-pointer mr-6' size={60} color='white' />
-            </a>
-            <a href="https://web.facebook.com/profile.php?id=100055735599274" target="_blank" rel="noopener noreferrer">
-              <FaFacebookF className='cursor-pointer mr-6' size={60} color='white' />
-            </a>
-          </div>
+                    </h2>
+                    
+                    <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+                      <a href="https://www.linkedin.com/in/sachini-kandambige/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className='cursor-pointer mr-6' size={60} color='white' />
+                      </a>
+                      <a href="https://github.com/IT21181856" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className='cursor-pointer mr-6' size={60} color='white' />
+                      </a>
+                      <a href="https://web.facebook.com/profile.php?id=100055735599274" target="_blank" rel="noopener noreferrer">
+                        <FaFacebookF className='cursor-pointer mr-6' size={60} color='white' />
+                      </a>
+                    </div> 
+                    
         </div>
+
       </div>
     </div>
   );
